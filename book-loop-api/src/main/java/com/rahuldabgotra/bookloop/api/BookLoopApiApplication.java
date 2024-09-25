d041bc39c980adda9b3af45ceb993935b14aba94
Rahul Dabgotra <rahuldabgo@gmail.com>
2024-09-25 19:35:46 +0530

    Initial commit

---
+++ book-loop-api/src/main/java/com/rahuldabgotra/bookloop/api/BookLoopApiApplication.java
@@ -0,0 +1,13 @@
+package com.rahuldabgotra.bookloop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoopApiApplication.class, args);
+	}
+
+}
---
+++ book-loop-api/src/test/java/com/rahuldabgotra/bookloop/api/BookLoopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahuldabgotra.bookloop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

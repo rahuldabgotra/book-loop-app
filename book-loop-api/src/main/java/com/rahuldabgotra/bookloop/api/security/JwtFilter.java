d0b70eab47a7ebe69dd069de9bd0609741ff10f9
Rahul Dabgotra <rahuldabgo@gmail.com>
2024-09-28 20:52:54 +0530

    Updates applying Security

---
+++ book-loop-api/src/main/java/com/rahuldabgotra/bookloop/api/security/JwtFilter.java
@@ -0,0 +1,4 @@
+package com.rahuldabgotra.bookloop.api.security;
+
+public class JwtFilter {
+}
--- book-loop-api/src/main/java/com/rahuldabgotra/bookloop/api/user/User.java
+++ book-loop-api/src/main/java/com/rahuldabgotra/bookloop/api/user/User.java
@@ -1,11 +1,14 @@
 package com.rahuldabgotra.bookloop.api.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rahuldabgotra.bookloop.api.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -13,6 +16,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static jakarta.persistence.FetchType.EAGER;
 
 @Getter
 @Setter
@@ -36,10 +42,16 @@ public class User implements UserDetails, Principal {
     private boolean accountLocked;
     private boolean enabled;
 
+    @ManyToMany(fetch = EAGER)
+    private List<Role> roles;
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
+
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
-
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
@@ -51,7 +63,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
